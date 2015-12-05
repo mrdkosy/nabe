16fd6262f74125a4b293bad75d480d054075c3c7
厚木麻耶 <s.and.m3.s2.s2@gmail.com>
2015-12-05 20:53:38 +0900

    7th
    7th

--- js/pastTime.js
+++ js/pastTime.js
@@ -20,14 +20,18 @@
             milles = parseInt(now.getTime() - start.getTime());
 
             console.log(sec);
-            if(sec == 0){
+            if(sec == 0){//多視点カメラ
                   $('#sceneMane').append('<script>setTimeout(function(){ Scene.goto("/scene2"); }, '+ (sec+2)+'000);<\/script>');
+            }else if(sec==1){
+                   $('#sceneMane').append('<script>setTimeout(function(){ Scene.goto("/scene0"); }, '+ (sec+2)+'000);<\/script>');
+            }else if(sec==3){
+                   $('#sceneMane').append('<script>setTimeout(function(){ Scene.goto("/scene4"); }, '+ (sec+2)+'000);<\/script>');
             }else if(sec%9 == 0){
                   $('#sceneMane').append('<script>setTimeout(function(){ Scene.goto("/scene2"); }, '+ (sec+2)+'000);<\/script>');
                   console.log("hello");
-            }else if(sec%6 == 0){
+            }else if(sec%6 == 0){//moise particle
                   $('#sceneMane').append('<script>setTimeout(function(){ Scene.goto("/scene4"); }, '+ (sec+2)+'000);<\/script>');
-            }else if(sec%3 == 0){
+            }else if(sec%3 == 0){//dragcircle
                   $('#sceneMane').append('<script>setTimeout(function(){ Scene.goto("/scene0"); }, '+ (sec+2)+'000);<\/script>');
             }
                   // window.location.reload();
